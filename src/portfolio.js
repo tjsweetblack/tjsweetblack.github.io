3bc0dee7857f2201d86f6ac8bd896bd39c447334
belmiro adriano <belmiroa34@gmail.com>
2024-09-25 16:14:03 -0400

    done

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ const greeting = {
     name: "Belmiro Adriano",
     message: "I'm a computer science student and an aspiring software engineer. Currently working as a web developer at my university building tools to aid students, educators, and staff.",
     basedLocation: "Angola, LUANDA",
-    resumeLink: "https://docs.google.com/document/d/1Sa_sK3RMusoezzgzxu8LkV16i2Jra2qd/edit?usp=sharing&ouid=113657039181859132688&rtpof=true&sd=true", // recommended: google drive file share link (change to "anyone on the internet can view")
+    resumeLink: "https://jmp.sh/nXo5fHJ4", // recommended: google drive file share link (change to "anyone on the internet can view")
     logo: {
         link: "images/initials.png", // use relative path from  parent directory -> ex: images/image.ext
         custom: true // takes precedence over image logo and allows for custom HTML logo (./components/Navbar.vue)
@@ -49,7 +49,10 @@ const about = {
         "Python",
         "Vue.js",
         "Dart",
-        "Flutter"
+        "Flutter",
+        "Github",
+        "Bash",
+        "CMake",
     ],
     photo1Link: "images/portrait1.jpeg",
     photo2Link: "images/portrait2.jpeg",
@@ -134,7 +137,7 @@ const contact = {
         ], // paragraph breaks will be entered after each item,
         link: {
             email: "", // email takes precedance
-            other: "https://forms.gle/W3MswTKsPWMxEbn58"
+            other: "https://forms.gle/wkePU4H9zH7nv82QA"
         },
         responseTimeMessage: "...and I'll resopnd within the next 24hrs"
     },
