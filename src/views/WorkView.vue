243ba955dffb63f7f1e43385855220a71aef0d2a
belmiro adriano <belmiro@debian.badriano>
2025-07-28 01:51:41 -0400

    one more time

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -14,18 +14,9 @@
             
             <!-- ProjectView components from projectUnit array -->
             <ProjectView
-                v-for="(project, index) in projInitial"
+                v-for="(project, index) in portfolio.projectUnit"
                 :key="`project-${index}`" :project="project"
             />
-            <ProjectView
-                v-if="showMore"
-                v-for="(project, index) in content"
-                :key="`project-more-${index}`" :project="project"
-            />
-
-            <button v-show="showPreview" @click="toggleShowMore" class="w-52 py-2 px-6 mx-auto mt-10 bg-transparent border border-button-color text-button-color shadow-sm shadow-button-color transition ease-in-out hover:bg-button-color hover:text-white focus:bg-button-color focus:text-white active:bg-button-color active:text-white duration-300">
-                {{ showMore ? "Show Less" : "Show More" }}
-            </button>
         </div>
     </section>
 </template>
@@ -33,29 +24,14 @@
 import { ref } from 'vue'
 import WorkUnit from '../components/WorkUnit.vue'
 import ProjectView from './ProjectView.vue'
-import { useRouter } from 'vue-router';
 import portfolio from '../portfolio';
 import { onIntersect } from '../composables/onIntersect';
-
-const router = useRouter()
 
 const props = defineProps({
     content: Object,
     transitions: Object
 })
 
-const showPreview = props.content.length > 3
-const showMore = ref(false)
-const projInitial = showPreview ? props.content.splice(0, 3) : props.content
-
 const workSection = ref({})
 const visible = props.transitions.active && window.matchMedia('(prefers-reduced-motion: no-preference)').matches ? onIntersect(workSection, !!props.transitions.showOnce, { threshold: props.transitions.thresholdOption }) : true
-
-let toggleShowMore = () => {
-    showMore.value = !showMore.value
-    if (!showMore.value) {
-        router.push('/#work-section')
-    }
-}
-
 </script>
